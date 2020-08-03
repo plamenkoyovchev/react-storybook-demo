0999cd1162da0f4b07b1685987664d91bea9b40c
plamenyovchev <plamenkooo@gmail.com>
2020-08-03 22:09:35 +0300

    Implement input story and put it inside Form hierarchy

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './Button';
 
 export default {
-    title: 'Button', // mandatory and should be unique inside the entire storybook
+    title: 'Form/Button', // mandatory and should be unique inside the entire storybook
     component: Button
 };
 
