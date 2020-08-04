5090f37387c16239600429997d7a6a1dd53671e1
plamenyovchev <plamenkooo@gmail.com>
2020-08-04 15:31:55 +0300

    Register global decorator

---
+++ .storybook/preview.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import Center from "../src/components/Center/Center";
+
+// Register global Decorator
+// addDecorator(story => <Center>{story()}</Center>);
